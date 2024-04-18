f15ea51c1e993bffec2af8a28c507562390b2e2c
justjudd <45545976+justjudd@users.noreply.github.com>
2024-04-17 17:32:03 -0700

    autoscaling

--- main.tf
+++ main.tf
@@ -29,21 +29,26 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  name = "blog"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  instance_type = var.instance_type
+  image_id      = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  version = "9.8.0"
 
   name = "blog-alb"
 
